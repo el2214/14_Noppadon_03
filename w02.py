d5333cf18b261d206bac0bd559d89fb52d622492
Sgt.Noppadon Sengboontan <el2214@signalschool.ac.th>
2025-06-19 15:22:35 +0700

    14_Noppadon_w03

---
+++ w02.py
@@ -0,0 +1,14 @@
+# input convert data type
+# frequency_str = input("กรุณากรอกความถี่ : ")
+# frequency_mhz = float(frequency_str)
+
+ # เทคนิค 01
+frequency_input =float(input("กรุณากรอกความถี่ : "))
+print(f"ความถี่ของคุณคือ = {frequency_input} ")
+
+# print(f"data type : {type(frequency_mhz)}{frequency_mhz}")
+# print(f"data type : {type(frequency_str)}{frequency_str}")
+
+ # เทคนิค 02
+show = f"ความถี่ของคุณคือ = {frequency_input}"
+print(show)
---
+++ w03.py
@@ -0,0 +1,10 @@
+# BMI Calculator
+# Result = W / {H^2 => H*H}
+
+W = float(input("น้ำหนัก(กก.)"))
+H = float(input("ส่วนสูง(ซม.)"))
+
+R = W/(H/100*H/100)
+R2 = f"{R:.2f}"
+print(f"{R:.2f}")
+print(R2)
