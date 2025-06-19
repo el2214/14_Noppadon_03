ea98fc173c25043ec2090439b3b247e43bc61a30
Sgt.Noppadon Sengboontan <el2214@signalschool.ac.th>
2025-06-19 15:54:13 +0700

    19-6-2568

---
+++ w06.py
@@ -0,0 +1,10 @@
+# List
+numbers = []
+
+colors = ["Red","Green","Blue","Yelloe"]
+numbers = [11,10,99,45,33,11]
+
+numbers_1_20 = list(range(1,21))
+numbers_1_100 = list(range(0,101,1))
+
+print(numbers_1_100)
---
+++ w07.py
@@ -0,0 +1,6 @@
+colors = ["Red","Green","Blue","Yelloe"]
+
+# print(f"{colors[0]}{colors[1]}{colors[2]}{colors[3]}")
+
+print(f"{colors[-4:]}")
+print(f"{colors[0:4]}")
